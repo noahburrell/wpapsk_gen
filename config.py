a301ec526a54aca27e44d8c87147a345e5a2b797
noah <noahburrell0@gmail.com>
2018-08-07 18:04:24 -0400

    Add debugging, change default PID path, add default configs for devices and interfaces

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import mysql.connector
 
+# Debugging
+debug = False
+
 # Save directory
 saveDir = "/tmp/"
 
@@ -8,7 +11,7 @@ privkey = "/home/noah/.ssh/id_rsa"
 pubkey = "/home/noah/.ssh/id_rsa.pub"
 
 # Location of hostapd keyfile location on gateway
-pidfile = "/var/run/wifi-phy1.pid"
+pidfile = "/var/run/wifi-phy0.pid"
 
 # Initialize connection to database
 database = mysql.connector.connect(
@@ -25,3 +28,27 @@ subnettable = "subnetTable"
 porttable = "portTable"
 devicetable = "deviceTable"
 gatewaytable = "gateways"
+
+# Default device configuration
+default_device_config = {
+        "wifi-device": "",  # DO NOT CHANGE IN THIS FILE
+        "type": "mac80211",
+        "hwmode": "11a",
+        "path": "pci0000:01/0000:01:00.0",
+        "country": "US",
+        "legacy_rates": "1",
+        "channel": "48",
+        "htmode": "HT20"
+    }
+
+# Default interface configuration
+default_iface_config = {
+    "wifi-iface": "",  # DO NOT CHANGE IN THIS FILE
+    "device": "",  # DO NOT CHANGE IN THIS FILE
+    "network": "lan",
+    "mode": "ap",
+    "ssid": "OpenWRT",
+    "encryption": "psk2",
+    "wpa_psk_file": "/etc/hostapd.wpa_psk",
+    "isolate": "1"
+}
