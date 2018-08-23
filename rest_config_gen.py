7c1a8f09a06945300b263f9bba7a4e49897f85a6
noah <noahburrell0@gmail.com>
2018-08-23 15:59:08 -0400

    Add service records location

--- rest_config_gen.py
+++ rest_config_gen.py
@@ -4,5 +4,6 @@ def generate(uid,server_address,xauth,debugging="False"):
             "uid = '"+str(uid)+"'\n"
             "server_ip = '"+str(server_address)+"'\n"
             "xauth_base = '"+str(xauth)+"'\n"
+            "service_records = \"/etc/service_records\"\n"
     )
     return device_config
